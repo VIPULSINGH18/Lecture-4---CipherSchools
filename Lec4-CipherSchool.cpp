b5068665107e2527c64ee082af873fa1075018af
VIPUL SINGH <120162007+VIPULSINGH18@users.noreply.github.com>
2024-06-10 13:26:19 +0530

    Add files via upload
    This File contains lecture 4 code of CipherSchool, where we learn how to take input and then perform arithmetic operation ...

---
+++ Lec4-CipherSchool.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    a= 5;
+    b= 3;
+    int sum;
+    sum = a+b;
+    cout<<"Sum of a and b :";
+    cout<<endl;
+    cout<<sum;
+
+    return 0;
+}
